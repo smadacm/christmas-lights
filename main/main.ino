bebb01e9e78f3306e5bacce281b43e39e68fbd4e
Christopher Adams <cadams@kadro.com>
2017-11-02 20:51:05 -0400

    prep for multiple strings, simple multi string

--- main/main.ino
+++ main/main.ino
@@ -3,45 +3,54 @@
   #include <avr/power.h>
 #endif
 
-#define PIN 6
+#define PIN_1 6
+#define PIN_2 4
 #define LED_COUNT 50
 
 // originally based on Adafruit's strandtest example:
 //   https://github.com/adafruit/Adafruit_NeoPixel/blob/312693bfce447095ff0d8b6f6a1cc569415d77d7/examples/strandtest/strandtest.ino
 // relies on libraries from the same repo
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip_1 = Adafruit_NeoPixel(LED_COUNT, PIN_1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip_2 = Adafruit_NeoPixel(LED_COUNT, PIN_2, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  strip_1.begin();
+  strip_1.show();
+  strip_2.begin();
+  strip_2.show();
 }
 
 void loop() {
-//  colorWipe(strip.Color(255, 0, 0), 50); // Red
-//  colorWipe(strip.Color(0, 255, 0), 50); // Green
+  colorWipe(strip_1, Color(0, 255, 0), 50); // Green
+  colorWipe(strip_2, Color(0, 255, 0), 50); // Green
+  colorWipe(strip_1, Color(255, 0, 0), 50); // Red
+  colorWipe(strip_2, Color(255, 0, 0), 50); // Red
 
-//  christmasChase(200, 10);
+  christmasChase(strip_1, 200, 10);
+  christmasChase(strip_2, 200, 10);
 
-  colorWipe(strip.Color(0, 100, 0), 50); // Green
-  dullWipe(strip.Color(255, 0, 0), strip.Color(0, 100, 0), 10, 3, 30); // Red on green
-  colorWipe(strip.Color(100, 0, 0), 50); // Red
-  dullWipe(strip.Color(0, 255, 0), strip.Color(100, 0, 0), 10, 3, 30); // Green on red
+  colorWipe(strip_1, Color(100, 0, 0), 50); // Red
+  colorWipe(strip_2, Color(100, 0, 0), 50); // Red
+  dullWipe(strip_1, Color(0, 255, 0), Color(100, 0, 0), 10, 3, 30); // Green on red
+  dullWipe(strip_2, Color(0, 255, 0), Color(100, 0, 0), 10, 3, 30); // Green on red
+  colorWipe(strip_1, Color(0, 100, 0), 50); // Green
+  colorWipe(strip_2, Color(0, 100, 0), 50); // Green
+  dullWipe(strip_1, Color(255, 0, 0), Color(0, 100, 0), 10, 3, 30); // Red on green
+  dullWipe(strip_2, Color(255, 0, 0), Color(0, 100, 0), 10, 3, 30); // Red on green
 
-  rainbow(20);
+  rainbow(strip_1, 20);
+  rainbow(strip_2, 20);
+  
+  colorWipe(strip_1, Color(100, 0, 0), 50); // Red
+  colorWipe(strip_2, Color(100, 0, 0), 50); // Red
+  colorWipe(strip_1, Color(0, 0, 0), 50); // Blank
+  colorWipe(strip_2, Color(0, 0, 0), 50); // Blank
 }
 
 
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-  }
-}
 
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbow(uint8_t wait) {
+void rainbow(Adafruit_NeoPixel &strip, uint8_t wait) {
   uint16_t i, j;
 
   for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
@@ -54,26 +63,26 @@ void rainbow(uint8_t wait) {
 }
 
 
-void christmasChase(uint8_t wait, int cycleCount) {
-  uint32_t red = strip.Color(255, 0, 0);
-  uint32_t green = strip.Color(0, 255, 0);
+void christmasChase(Adafruit_NeoPixel &strip, uint8_t wait, int cycleCount) {
+  uint32_t red = Color(255, 0, 0);
+  uint32_t green = Color(0, 255, 0);
   for (int j=0; j<cycleCount; j++) {  //do 10 cycles of chasing
     for (int q=0; q < 3; q++) {
       for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, red);    //turn every third pixel on
+        strip.setPixelColor(i+q, green);    //turn every third pixel on
       }
       strip.show();
 
       delay(wait);
 
       for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, green);        //turn every third pixel off
+        strip.setPixelColor(i+q, red);        //turn every third pixel off
       }
     }
   }
 }
 
-void dullWipe(uint32_t fgColor, uint32_t bgColor, int cycles, int width, int wait){
+void dullWipe(Adafruit_NeoPixel &strip, uint32_t fgColor, uint32_t bgColor, int cycles, int width, int wait){
   for(int i=0; i < cycles; i++){
     int fgPixel=(0 - (width - 1));
     for(; fgPixel < strip.numPixels() || fgPixel < 0; fgPixel++){
@@ -91,18 +100,34 @@ void dullWipe(uint32_t fgColor, uint32_t bgColor, int cycles, int width, int wai
 }
 
 
+/* Straight from Adafruit lib */
+
+void colorWipe(Adafruit_NeoPixel &strip, uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Convert separate R,G,B into packed 32-bit RGB color.
+// Packed format is always RGB, regardless of LED strand color order.
+uint32_t Color(uint8_t g, uint8_t r, uint8_t b) {
+  return ((uint32_t)r << 16) | ((uint32_t)g <<  8) | b;
+}
+
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    return Color(255 - WheelPos * 3, 0, WheelPos * 3);
   }
   if(WheelPos < 170) {
     WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    return Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
   WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
